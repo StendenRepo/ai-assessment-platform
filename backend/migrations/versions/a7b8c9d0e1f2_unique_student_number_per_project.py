e9d5f7addc44a504c7c1e2028de62841be1e58df
Cedric Smit <cedsmit@gmail.com>
2026-06-03 02:37:47 +0200

    Fix down_revision reference in unique student number migration and remove obsolete seed demo project migration

--- backend/migrations/versions/a7b8c9d0e1f2_unique_student_number_per_project.py
+++ backend/migrations/versions/a7b8c9d0e1f2_unique_student_number_per_project.py
@@ -1,7 +1,7 @@
 """unique student number per project
 
 Revision ID: a7b8c9d0e1f2
-Revises: f6a7b8c9d0e1
+Revises: e5f6a7b8c9d0
 Create Date: 2026-06-01 09:30:00.000000
 
 """
@@ -10,7 +10,7 @@ from typing import Sequence, Union
 from alembic import op
 
 revision: str = "a7b8c9d0e1f2"
-down_revision: Union[str, None] = "f6a7b8c9d0e1"
+down_revision: Union[str, None] = "e5f6a7b8c9d0"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
